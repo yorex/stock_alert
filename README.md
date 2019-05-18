693cd53fab0b13a0012390e569fd6dd193d5cd19
qiul.yaoql@alibaba-inc.com <qiul.yaoql@alibaba-inc.com>
2019-05-18 16:43:51 +0800

    改用js

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # stock_alert
 stock alert
+
+
+# echart.js
+https://echarts.baidu.com/option.html#tooltip
---
+++ start.sh
@@ -0,0 +1,4 @@
+export FLASK_APP=dashboard.py
+export FLASK_ENV=development
+
+flask run --host=0.0.0.0 --port=5000
