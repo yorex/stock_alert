5a83688d80eb925a72f0cf3f62c0cf0081ac4c01
yorex <huair777@163.com>
2019-12-18 21:33:11 +0800

    基于chrome采集指定列表stock数据

---
+++ crawler/crawler.py
@@ -0,0 +1,73 @@
+#! /usr/local/bin/python
+# encoding: utf-8
+
+import time
+import datetime
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import sys; sys.path.append("../")
+from mongoHelper import MongoHelper
+import logger
+
+
+class YahooCrawler:
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        self.DRIVER = webdriver.Chrome(chrome_options=chrome_options)
+        self.mongo = MongoHelper("stock")
+
+    def __del__(self):
+        self.DRIVER.quit()
+
+    def to_float(self, s):
+        return float(s.replace(",", ""))
+
+    def removeDotHK(self, subject):
+        return subject.split(".")[0]
+
+    def _save_data(self, subject, datas):
+        logger.info("save_data:%s", datas)
+        date= time.strftime("%Y%m%d", time.strptime(datas[0].encode("utf-8"), "%Y年%m月%d日"))
+        dopen = self.to_float(datas[1])
+        dmax = self.to_float(datas[2])
+        dmin = self.to_float(datas[3])
+        dclose = self.to_float(datas[4])
+        volume = self.to_float(datas[5])
+        data={
+            "date":date, 
+            "dopen":dopen, 
+            "dmax":dmax, 
+            "dmin":dmin, 
+            "dclose":dclose, 
+            "volume":volume
+        }
+        collectionName = self.removeDotHK(subject)
+        self.mongo.update(collectionName, {"date":date}, data, True)
+    
+    # 获取 [date_begin, date_end) 内的数据, date格式为%Y%m%d
+    def fetch(self, subject, date_begin, date_end):
+        epoch_begin = int(time.mktime(time.strptime(date_begin, "%Y%m%d")))
+        epoch_end = int(time.mktime(time.strptime(date_end, "%Y%m%d")))
+        url = "https://hk.finance.yahoo.com/quote/%s/history?period1=%s&period2=%s&interval=1d&filter=history&frequency=1d" % (subject, epoch_begin, epoch_end)
+        logger.info("url:%s", url)
+        self.DRIVER.get(url)
+        trs = self.DRIVER.find_element_by_xpath("//table//tbody").find_elements_by_tag_name("tr")
+        for tr in trs:
+            tds = tr.find_elements_by_tag_name("td")
+            self._save_data(subject, [td.text for td in tds])
+
+
+if __name__ == "__main__":
+    today = datetime.date.today()
+    oneday = datetime.timedelta(days=1)
+    yesterday = today - oneday
+    tomorrow = today + oneday
+    
+    for subject in  open("subjects.conf", "r"): 
+        yahooCrawler = YahooCrawler();
+        yahooCrawler.fetch(subject.strip(), yesterday.strftime("%Y%m%d"), tomorrow.strftime("%Y%m%d"));
+        del yahooCrawler
+
+ 
--- crawler/headless_chrome_demo.py
+++ crawler/headless_chrome_demo.py
@@ -25,7 +25,8 @@ def close_web_driver():
     DRIVER.quit() 
     
 def get_data():
-    url = "https://hk.finance.yahoo.com/quote/%5EHSI/history?ltr=1&guccounter=1&guce_referrer=aHR0cHM6Ly94dWVxaXUuY29tL1MvSEtIU0k&guce_referrer_sig=AQAAAGtP5FNuYYONKlKt1JtDLAgcUo-MVNSIxtMO_BxUBIFKPizNuHf4vtkm02FxryW_4EUcsB8rGo_U6uh7ZKrhB_uCdnbHa_3uduuzCYCrACfVPPBgD5AyoZ5LecKNdeVKOv9sRpvXyaMuruL3J-GFKBSIHYxmJEwnrZR5rrhiFMR9"
+    #url = "https://hk.finance.yahoo.com/quote/%5EHSI/history?ltr=1&guccounter=1&guce_referrer=aHR0cHM6Ly94dWVxaXUuY29tL1MvSEtIU0k&guce_referrer_sig=AQAAAGtP5FNuYYONKlKt1JtDLAgcUo-MVNSIxtMO_BxUBIFKPizNuHf4vtkm02FxryW_4EUcsB8rGo_U6uh7ZKrhB_uCdnbHa_3uduuzCYCrACfVPPBgD5AyoZ5LecKNdeVKOv9sRpvXyaMuruL3J-GFKBSIHYxmJEwnrZR5rrhiFMR9"
+    url = "https://hk.finance.yahoo.com/quote/9988.HK/history?period1=1576425600&period2=1576598400&interval=1d&filter=history&frequency=1d"
     DRIVER.get(url)
     tds = DRIVER.find_element_by_xpath("//table//tbody").find_elements_by_tag_name("td")
     for td in tds:
